58dbcf81883416caca109cf82bfd90a0bf825a45
J4N8 <jan.berlocnik@gmail.com>
2021-08-23 20:55:53 +0200

    Added database connection

---
+++ src/main/java/com/j4n8/economy/Database.java
@@ -0,0 +1,54 @@
+package com.j4n8.economy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database extends JavaPlugin {
+    private Connection connection;
+    public String host, database, username, password;
+    public int port;
+
+    public void onEnable() {
+        mysqlSetup();
+    }
+
+    public void mysqlSetup(){
+        host = "localhost";
+        port = 3306;
+        database = "economy";
+        username = "root";
+        password = "password";
+
+        try{
+
+            synchronized (this){
+                if(getConnection() != null && !getConnection().isClosed()){
+                    return;
+                }
+
+                Class.forName("com.mysql.jdbc.Driver");
+                setConnection( DriverManager.getConnection("jdbc:mysql://" + this.host + ":"
+                        + this.port + "/" + this.database, this.username, this.password));
+
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "MYSQL CONNECTED");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
