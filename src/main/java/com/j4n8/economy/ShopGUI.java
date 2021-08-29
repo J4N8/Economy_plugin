2354216168cc1a2041b89593d6126710e6a0d1b3
J4N8 <43821141+J4N8@users.noreply.github.com>
2021-08-29 22:10:07 +0200

    Added shop GUI Shop GUI is now configurable

--- src/main/java/com/j4n8/economy/ShopGUI.java
+++ src/main/java/com/j4n8/economy/ShopGUI.java
@@ -3,6 +3,8 @@ package com.j4n8.economy;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,13 +16,19 @@ import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class ShopGUI implements Listener {
+    private final Plugin plugin;
     private final Inventory shopGUI;
-    public ShopGUI() {
-        shopGUI = Bukkit.createInventory(null, 9, "Shop");
+    public ShopGUI(Plugin plugin) {
+        this.plugin = plugin;
+        shopGUI = Bukkit.createInventory(null, 54, "Shop");
 
         // Put the items into the inventory
         initializeItems();
@@ -28,17 +36,30 @@ public class ShopGUI implements Listener {
 
     // You can call this whenever you want to put the items in
     public void initializeItems() {
-        shopGUI.addItem(createGuiItem(Material.DIAMOND_SWORD, 1, "Example Sword", "§aFirst line of the lore", "§bSecond line of the lore"));
-        shopGUI.addItem(createGuiItem(Material.IRON_HELMET, 1, "§bExample Helmet", "§aFirst line of the lore", "§bSecond line of the lore"));
+        FileConfiguration config = plugin.getConfig();
+        ConfigurationSection section = config.getConfigurationSection("shop");
+        Set<String> categories = section.getKeys(false);
+        for (String item : categories){
+            shopGUI.addItem(createGuiItem(Material.getMaterial(item), config.getInt("shop."+item+".amount"), "Price: "+config.getInt("shop."+item+".price")));
+        }
     }
 
     // Nice little method to create a gui item with a custom name, and description
-    protected ItemStack createGuiItem(final Material material, final int amount, final String name, final String... lore) {
+    protected ItemStack createGuiItem(final String name, final Material material, final int amount, final String... lore) {
         final ItemStack item = new ItemStack(material, amount);
         final ItemMeta meta = item.getItemMeta();
 
         // Set the name of the item
         meta.setDisplayName(name);
+        // Set the lore of the item
+        meta.setLore(Arrays.asList(lore));
+        item.setItemMeta(meta);
+        return item;
+    }
+    protected ItemStack createGuiItem(final Material material, final int amount, final String... lore) {
+        final ItemStack item = new ItemStack(material, amount);
+        final ItemMeta meta = item.getItemMeta();
+
         // Set the lore of the item
         meta.setLore(Arrays.asList(lore));
         item.setItemMeta(meta);
@@ -56,18 +77,18 @@ public class ShopGUI implements Listener {
         if (!e.getInventory().getClass().equals(shopGUI.getClass())){
             return;
         }
-
         e.setCancelled(true);
 
         final ItemStack clickedItem = e.getCurrentItem();
-
         // verify current item is not null
         if (clickedItem == null || clickedItem.getType().isAir()) return;
-
         final Player p = (Player) e.getWhoClicked();
+        Inventory inv = p.getInventory();
 
-        // Using slots click is the best option for your inventory click's
+        //Check which slot was clicked
         p.sendMessage("You clicked at slot " + e.getRawSlot());
+        inv.setItem(inv.firstEmpty(), clickedItem);
+        Database.increasePlayerBalance(p, Integer.parseInt(clickedItem.getLore().get(0).split("Price: ")[1]));
     }
 
     @EventHandler
